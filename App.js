00774b26fcccecee38fc48aecd59899b25babfca
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2018-08-14 13:29:12 +0300

    Ignore warning

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import {
 import HomeScreen from './HomeScreen';
 import EditScreen from './EditScreen';
 
+//TODO: remove this ignore when upgrading react native to 0.57
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
 const App = createStackNavigator({
   Home: { screen: HomeScreen },
   Edit: { screen: EditScreen },
